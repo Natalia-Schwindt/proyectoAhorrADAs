02e87fc957c4f05e10e84f5b23b53dfee1f1a038
MarinaArrieta <arrietamarina12@gmail.com>
2024-05-13 20:18:21 -0300

    Funcion ocultar para filtros

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,9 +4,15 @@ const balanceFiltros = document.getElementById('balanceFiltros');
 const operaciones = document.getElementById('operaciones');
 const botonNavHamburguesa = document.getElementById('boton-nav-hamburguesa');
 const ulNavHeader = document.getElementById('ul-nav-header');
+const ocultarFiltros = document.getElementById('ocultar-filtros');
+const formFiltros = document.getElementById('form-filtros');
 
 botonNavHamburguesa.addEventListener('click', () => {
     ulNavHeader.classList.toggle('hidden');
+});
+
+ocultarFiltros.addEventListener('click', () => {
+    formFiltros.classList.toggle('hidden');
 });
 
 nuevaOperacion.addEventListener('click', () => {
