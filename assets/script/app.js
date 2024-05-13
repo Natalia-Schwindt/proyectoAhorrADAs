4460023204f66da082ddf2ccf2b5da5e4bc6a9f6
MarinaArrieta <arrietamarina12@gmail.com>
2024-05-13 16:16:11 -0300

    evento click para abrir y cerrar el nav

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,6 +2,12 @@ const nuevaOperacion = document.getElementById('botonNuevaOperacion');
 const ventanaNuevaOperacion = document.getElementById('ventanaNuevaOperacion');
 const balanceFiltros = document.getElementById('balanceFiltros');
 const operaciones = document.getElementById('operaciones');
+const botonNavHamburguesa = document.getElementById('boton-nav-hamburguesa');
+const ulNavHeader = document.getElementById('ul-nav-header');
+
+botonNavHamburguesa.addEventListener('click', () => {
+    ulNavHeader.classList.toggle('hidden');
+});
 
 nuevaOperacion.addEventListener('click', () => {
     ventanaNuevaOperacion.style.display = 'block';
