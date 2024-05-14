be6c6c72a66e6ccbc6419af9f46d962bd1b29cef
Natalia-Schwindt <naty8014@gmail.com>
2024-05-14 12:27:20 -0300

    agregando funcionalidad y modificando estructura

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,19 +1,14 @@
-const nuevaOperacion = document.getElementById('botonNuevaOperacion');
-const ventanaNuevaOperacion = document.getElementById('ventanaNuevaOperacion');
-const balanceFiltros = document.getElementById('balanceFiltros');
-const operaciones = document.getElementById('operaciones');
 const botonNavHamburguesa = document.getElementById('boton-nav-hamburguesa');
 const ulNavHeader = document.getElementById('ul-nav-header');
-const ocultarFiltros = document.getElementById('ocultar-filtros');
-const formFiltros = document.getElementById('form-filtros');
 
 botonNavHamburguesa.addEventListener('click', () => {
     ulNavHeader.classList.toggle('hidden');
 });
 
-ocultarFiltros.addEventListener('click', () => {
-    formFiltros.classList.toggle('hidden');
-});
+const nuevaOperacion = document.getElementById('botonNuevaOperacion');
+const ventanaNuevaOperacion = document.getElementById('ventanaNuevaOperacion');
+const balanceFiltros = document.getElementById('balanceFiltros');
+const operaciones = document.getElementById('operaciones');
 
 nuevaOperacion.addEventListener('click', () => {
     ventanaNuevaOperacion.style.display = 'block';
