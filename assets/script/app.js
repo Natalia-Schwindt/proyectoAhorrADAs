e1aee4f7d9ac3413a613f1ca938bbcb00e1c9010
MarinaArrieta <arrietamarina12@gmail.com>
2024-05-14 15:38:18 -0300

    link de HTML de reporter y categorias en index, acomodo de varibles en js, funcionamento del nav en pantallas de categoria y reportes, funcionamiento de ocultar filtros

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,14 +1,19 @@
 const botonNavHamburguesa = document.getElementById('boton-nav-hamburguesa');
 const ulNavHeader = document.getElementById('ul-nav-header');
+const nuevaOperacion = document.getElementById('botonNuevaOperacion');
+const ventanaNuevaOperacion = document.getElementById('ventanaNuevaOperacion');
+const balanceFiltros = document.getElementById('balanceFiltros');
+const operaciones = document.getElementById('operaciones');
+const ocultarFiltros = document.getElementById('ocultarFiltros');
+const formFiltros = document.getElementById('formFiltros');
 
 botonNavHamburguesa.addEventListener('click', () => {
     ulNavHeader.classList.toggle('hidden');
 });
 
-const nuevaOperacion = document.getElementById('botonNuevaOperacion');
-const ventanaNuevaOperacion = document.getElementById('ventanaNuevaOperacion');
-const balanceFiltros = document.getElementById('balanceFiltros');
-const operaciones = document.getElementById('operaciones');
+ocultarFiltros.addEventListener('click', () => {
+    formFiltros.classList.toggle('hidden');
+});
 
 nuevaOperacion.addEventListener('click', () => {
     ventanaNuevaOperacion.style.display = 'block';
--- assets/script/categorias.js
+++ assets/script/categorias.js
@@ -1,6 +1,12 @@
 const botonesEditar = document.querySelectorAll('.botonEditar');
 const categorias = document.getElementById('categorias');
 const editarCategoria = document.getElementById('editarCategoria');
+const botonNavHamburguesa = document.getElementById('boton-nav-hamburguesa');
+const ulNavHeader = document.getElementById('ul-nav-header');
+
+botonNavHamburguesa.addEventListener('click', () => {
+    ulNavHeader.classList.toggle('hidden');
+});
 
 botonesEditar.forEach(botonEditar => {
     botonEditar.addEventListener('click', () =>{
